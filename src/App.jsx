5ff0aae0267ceedc301bd52251133428fb235aba
Vinny021 <viniciusbarbosa021@gmail.com>
2023-10-17 00:09:28 -0300

    Feat: display do numero

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [currentNumber, setCurrentNumber] = useState(0);
+
+
+  const updateCurrentNumber = (event) => {
+    const newNumber = event.target.value.length === 0 ? 0 : event.target.value;
+    setCurrentNumber(parseInt(newNumber));
+  }
+
   return (
     <div className="App">
+      <input id="display" className="Display" type="number" onChange={updateCurrentNumber} value={currentNumber}/>
     </div>
   );
 }
