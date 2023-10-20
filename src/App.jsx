a7a95405862390e93522bd8535fdc575a7824746
Vinny021 <viniciusbarbosa021@gmail.com>
2023-10-20 00:00:20 -0300

    Feat: Calculator layout

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,30 @@ function App() {
       <div className='CalculatorBase'>
         <input id="display" className="Display" type="number" onChange={updateCurrentNumber} value={currentNumber}/>
         <div className='KeyboardSection'>
-          
+          <div className='ButtonsRow'>
+            <div className='Button'>7</div>
+            <div className='Button'>8</div>
+            <div className='Button'>9</div>
+            <div className='Button'>÷</div>
+          </div>
+          <div className='ButtonsRow'>
+            <div className='Button'>4</div>
+            <div className='Button'>5</div>
+            <div className='Button'>6</div>
+            <div className='Button'>x</div>
+          </div>
+          <div className='ButtonsRow'>
+            <div className='Button'>1</div>
+            <div className='Button'>2</div>
+            <div className='Button'>3</div>
+            <div className='Button'>-</div>
+          </div>
+          <div className='ButtonsRow'>
+            <div className='Button'>0</div>
+            <div className='Button'>.</div>
+            <div className='Button'>=</div>
+            <div className='Button'>+</div>
+          </div>
         </div>
       </div>
     </div>
