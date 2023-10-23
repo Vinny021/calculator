f96fe6d970bfb94de41da60c568bc3232c25fdf6
Vinny021 <viniciusbarbosa021@gmail.com>
2023-10-22 23:32:54 -0300

    Feat: Input de numeros

--- src/App.jsx
+++ src/App.jsx
@@ -10,34 +10,49 @@ function App() {
     setCurrentNumber(parseInt(newNumber));
   }
 
+  const numberInput = (event) => {
+    const currentNumberString = currentNumber.toString();
+    if(currentNumberString.length < 8){
+      const inputedNumber = event.target.innerText;
+      const newNumber = parseInt(currentNumberString + inputedNumber);
+      setCurrentNumber(newNumber);
+    }
+  }
+
   return (
     <div className="App">
       <div className='CalculatorBase'>
         <input id="display" className="Display" type="number" onChange={updateCurrentNumber} value={currentNumber}/>
         <div className='KeyboardSection'>
           <div className='ButtonsRow'>
-            <div className='Button'>7</div>
-            <div className='Button'>8</div>
-            <div className='Button'>9</div>
-            <div className='Button'>÷</div>
+            <div className='Button' style={{backgroundColor:'#454a5400'}}></div>
+            <div className='Button' style={{backgroundColor:'#454a5400'}}></div>
+            <div className='Button' style={{backgroundColor:'#454a5400'}}></div>
+            <button className='Button'>AC</button>
           </div>
           <div className='ButtonsRow'>
-            <div className='Button'>4</div>
-            <div className='Button'>5</div>
-            <div className='Button'>6</div>
-            <div className='Button'>x</div>
+            <button onClick={numberInput} className='Button'>7</button>
+            <button onClick={numberInput} className='Button'>8</button>
+            <button onClick={numberInput} className='Button'>9</button>
+            <button className='Button'>÷</button>
           </div>
           <div className='ButtonsRow'>
-            <div className='Button'>1</div>
-            <div className='Button'>2</div>
-            <div className='Button'>3</div>
-            <div className='Button'>-</div>
+            <button onClick={numberInput} className='Button'>4</button>
+            <button onClick={numberInput} className='Button'>5</button>
+            <button onClick={numberInput} className='Button'>6</button>
+            <button className='Button'>X</button>
           </div>
           <div className='ButtonsRow'>
-            <div className='Button'>0</div>
-            <div className='Button'>.</div>
-            <div className='Button'>=</div>
-            <div className='Button'>+</div>
+            <button onClick={numberInput} className='Button'>1</button>
+            <button onClick={numberInput} className='Button'>2</button>
+            <button onClick={numberInput} className='Button'>3</button>
+            <button className='Button'>-</button>
+          </div>
+          <div className='ButtonsRow'>
+            <button onClick={numberInput} className='Button'>0</button>
+            <button className='Button'>.</button>
+            <button className='Button'>=</button>
+            <button className='Button'>+</button>
           </div>
         </div>
       </div>
